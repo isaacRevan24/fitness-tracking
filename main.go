c357a180bde5f4f9e3f0b22079980c794e70be80
Severiano Atencio <satencio@consultec-ti.com>
2022-06-25 21:49:03 -0500

    add: base controller

--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/isaacRevan24/fitness-tracking/controller"
+)
+
+var (
+	controllerRegister controller.ControllerRegisterInterface = controller.NewController()
 )
 
 func main() {
 	r := gin.Default()
+	v1 := r.Group("/v1")
+	controllerRegister.TrackingController(v1.Group("/tracking"))
 	r.Run()
 }
