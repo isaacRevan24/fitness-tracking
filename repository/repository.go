36fadbd8e274de960b37ed3ee354197ec9a28285
Severiano Atencio <satencio@consultec-ti.com>
2022-06-26 22:32:13 -0500

    add: base repository

---
+++ repository/repository.go
@@ -0,0 +1,55 @@
+package repository
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/google/uuid"
+	"github.com/isaacRevan24/fitness-tracking/model"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "root"
+	password = "postgres"
+	dbname   = "fitness"
+)
+
+type repo struct {
+	db *sql.DB
+}
+
+type TrackingRepository interface {
+	AddWeightRegister(request *model.AddWeightRegisterReq) (string, error)
+}
+
+func NewTrackingRepository() TrackingRepository {
+	repo, _ := getConnection()
+	return repo
+}
+
+func getConnection() (*repo, error) {
+
+	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname))
+
+	if err != nil {
+		log.Fatal("Failed to open a DB connection: ", err)
+		return nil, err
+	}
+
+	return &repo{db: db}, nil
+}
+
+func (r *repo) AddWeightRegister(request *model.AddWeightRegisterReq) (string, error) {
+	defer r.db.Close()
+	sqlStatement := `INSERT INTO weight_track(id, weight, created_at) VALUES($1, $2, $3) RETURNING weight_track_id`
+	id := uuid.NullUUID{}
+	insertError := r.db.QueryRow(sqlStatement, &request.ClientId, &request.Weight, &request.CreatedAt).Scan(id)
+	if insertError != nil {
+		return "", insertError
+	}
+	return id.UUID.String(), nil
+}
