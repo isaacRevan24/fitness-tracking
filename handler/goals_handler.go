fd86266354b184ba76c7144a06d5c6ac9731d7d1
Severiano Atencio <satencio@consultec-ti.com>
2022-10-07 15:07:08 -0500

    change: refactor files de handler

---
+++ handler/goals_handler.go
@@ -0,0 +1,20 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/isaacRevan24/fitness-tracking/model"
+)
+
+func (handler goalsHandler) AddGoalsRegisters(request model.AddWeightGoalsReq) model.FitnessResponse {
+	var response model.FitnessResponse
+	register, error := GoalsRepo.AddGoalsRegisters(request)
+	if error != nil {
+		responseStatus := mapper.ToBaseStatus(http.StatusBadRequest, model.BAD_REQUEST_ERROR_STATUS, model.ADD_GOALS_REGISTER_ERROR)
+		mapper.ToFitnessResponse(&response, responseStatus, nil)
+		return response
+	}
+	status := mapper.ToBaseStatus(http.StatusOK, model.SUCCESS_CODE_STATUS, model.SUCCESS_MESSAGE)
+	mapper.ToFitnessResponse(&response, status, register)
+	return response
+}
--- handler/handler.go
+++ handler/handler.go
@@ -2,8 +2,6 @@
 package handler
 
 import (
-	"net/http"
-
 	"github.com/isaacRevan24/fitness-tracking/model"
 	"github.com/isaacRevan24/fitness-tracking/repository"
 )
@@ -35,70 +33,4 @@ func NewTrackingHandler() TrackingHandlerInterface {
 
 func NewGoalsHandler() GoalsHandlerInterface {
 	return goalsHandler{}
-}
-
-func (handler trackingHandler) AddWeightRegister(request model.AddWeightRegisterReq) model.FitnessResponse {
-	var response model.FitnessResponse
-	id, error := Repo.AddWeightRegister(request)
-	if error != nil {
-		responseStatus := mapper.ToBaseStatus(http.StatusBadRequest, model.BAD_REQUEST_ERROR_STATUS, model.ADD_WEIGHT_REGISTER_ERROR)
-		mapper.ToFitnessResponse(&response, responseStatus, nil)
-		return response
-	}
-	responseStatus := mapper.ToBaseStatus(http.StatusOK, model.SUCCESS_CODE_STATUS, model.SUCCESS_MESSAGE)
-	responseBody := model.AddWeightRegisterRes{WeightTrackId: id}
-	mapper.ToFitnessResponse(&response, responseStatus, responseBody)
-	return response
-}
-
-func (handler trackingHandler) GetWeightRegister(clientId string, weightId string) model.FitnessResponse {
-	var response model.FitnessResponse
-	register, error := Repo.GetWeightRegister(clientId, weightId)
-	if error != nil {
-		responseStatus := mapper.ToBaseStatus(http.StatusBadRequest, model.BAD_REQUEST_ERROR_STATUS, model.GET_WEIGHT_REGISTER_ERROR)
-		mapper.ToFitnessResponse(&response, responseStatus, nil)
-		return response
-	}
-	responseStatus := mapper.ToBaseStatus(http.StatusOK, model.SUCCESS_CODE_STATUS, model.SUCCESS_MESSAGE)
-	mapper.ToFitnessResponse(&response, responseStatus, register)
-	return response
-}
-
-func (handler trackingHandler) UpdateWeightRegister(request model.UpdateWeightRegisterReq) model.FitnessResponse {
-	var response model.FitnessResponse
-	error := Repo.UpdateWeightRegister(request)
-	if error != nil {
-		responseStatus := mapper.ToBaseStatus(http.StatusBadRequest, model.BAD_REQUEST_ERROR_STATUS, model.UPDATE_WEIGHT_REGISTER_ERROR)
-		mapper.ToFitnessResponse(&response, responseStatus, nil)
-		return response
-	}
-	responseStatus := mapper.ToBaseStatus(http.StatusOK, model.SUCCESS_CODE_STATUS, model.SUCCESS_MESSAGE)
-	mapper.ToFitnessResponse(&response, responseStatus, nil)
-	return response
-}
-
-func (handler trackingHandler) DeleteWeightRegister(request model.DeleteWeightRegisterReq) model.FitnessResponse {
-	var response model.FitnessResponse
-	error := Repo.DeleteWeightRegister(request)
-	if error != nil {
-		responseStatus := mapper.ToBaseStatus(http.StatusBadRequest, model.BAD_REQUEST_ERROR_STATUS, model.DELETE_WEIGHT_REGISTER_ERROR)
-		mapper.ToFitnessResponse(&response, responseStatus, nil)
-		return response
-	}
-	responseStatus := mapper.ToBaseStatus(http.StatusOK, model.SUCCESS_CODE_STATUS, model.SUCCESS_MESSAGE)
-	mapper.ToFitnessResponse(&response, responseStatus, nil)
-	return response
-}
-
-func (handler goalsHandler) AddGoalsRegisters(request model.AddWeightGoalsReq) model.FitnessResponse {
-	var response model.FitnessResponse
-	register, error := GoalsRepo.AddGoalsRegisters(request)
-	if error != nil {
-		responseStatus := mapper.ToBaseStatus(http.StatusBadRequest, model.BAD_REQUEST_ERROR_STATUS, model.ADD_GOALS_REGISTER_ERROR)
-		mapper.ToFitnessResponse(&response, responseStatus, nil)
-		return response
-	}
-	status := mapper.ToBaseStatus(http.StatusOK, model.SUCCESS_CODE_STATUS, model.SUCCESS_MESSAGE)
-	mapper.ToFitnessResponse(&response, status, register)
-	return response
 }
